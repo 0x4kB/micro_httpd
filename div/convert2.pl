ca99828a63884039b5cd080106896cfa7e72c124
misc147 <misc7@posteo.net>
2025-04-07 00:36:26 +0000

    add mimetypes

--- div/convert2.pl
+++ div/convert2.pl
@@ -3,14 +3,21 @@
 
 open F,shift or die;
 
+our %h = ( "application"=>1, 'audio'=>3, 'image'=>4, 'video'=>5, 'text'=>6 );
+#print $h{"application"};
+#exit;
+
 
 foreach my $l ( <F> ){
 	$l =~ /^([^,]*),([^,]*),([^,]*),([^,]*)/;
 	our @a = ( $1,$2,$3,$4 );
+	$a[1] =~ /^([^\/]*)/;
+	$class = $1;
 	$a[1] =~ s/[\/\-]/_/g;
 	chomp $a[3];
+
 	
-	print "   _MIME( $a[0], $a[1], \"$a[2]\" )  /* $a[3] */ \\\n"
+	print "   _MIME( $a[0], $a[1], \"\\$h{$class}$a[2]\" )  /* $a[3] */ \\\n"
 
 
 }
